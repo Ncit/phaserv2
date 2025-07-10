1c7b4bd6fcb0584e59e06206b5e68be9102c46c9
ncit <nikmobdev@gmail.com>
2025-07-10 08:29:40 +0300

    - fixes

--- pokerv2/src/scenes/Start.js
+++ pokerv2/src/scenes/Start.js
@@ -31,27 +31,15 @@ export class Start extends Phaser.Scene {
         
         // Load bonus button asset
         this.load.image('bonus_button', 'assets/bonus_button.png');
-  vkBridge.send('VKWebAppGetLaunchParams')
-  .then((data) => { 
-    if (data.vk_user_id) {
-      userInfo(data.vk_user_id, function(authData) {
-        console.log(authData.photo_200)
-      this.load.image('avatarQ', authData.photo_200);
-});
-    }
-  })
-  .catch((error) => {
-    // Ошибка
-    console.log(error);
-  });
-  
-        // setupApp(function(appData) {
-        //         console.log(appData);
-        //              this.load.image('avatarQ', appData.photo_200);
+
+        setupApp(function(appData) {
+                console.log("----");
+                console.log(appData);
+                     this.load.image('avatarQ', appData.photo_200);
    
-        //         // this.userAvatar = this.add.image(120, 46, appData.photo_200);
-        //         // this.userAvatar.scale = 0.1;
-        // });
+                // this.userAvatar = this.add.image(120, 46, appData.photo_200);
+                // this.userAvatar.scale = 0.1;
+        });
 
         // this.load.image('avatarQ', 'https://gravatar.com/avatar/2ee1f504b415b376c586641aee2c3194?s=400&d=robohash&r=x');
     }
@@ -98,7 +86,6 @@ export class Start extends Phaser.Scene {
 
                 this.userAvatar = this.add.image(120, 46, 'avatarQ');
                 this.userAvatar.scale = 0.1;
-                
         this.chipButton.scale = 0.35;
         this.chipLabel.setTint(0xffffff);
         this.chipLabel.setAlpha(0.22);
