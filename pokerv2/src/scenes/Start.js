baa7895658b71887638b48033ee412ae755f75d9
ncit <nikmobdev@gmail.com>
2025-07-10 09:53:09 +0300

    sd

--- pokerv2/src/scenes/Start.js
+++ pokerv2/src/scenes/Start.js
@@ -77,7 +77,7 @@ export class Start extends Phaser.Scene {
         // this.progress.scale = 0.34;
         // this.star.scale = 0.36;
 this.time.addEvent({
-        delay: 750,
+        delay: 1000,
         loop: false,
         callback: () => {
            this.userAvatar = this.add.image(120, 46, 'avatarQ');
