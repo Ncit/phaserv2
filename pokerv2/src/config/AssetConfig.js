59ee22df725ac3e9b84a5b5b5d290425fceff4ff
ncit <nikmobdev@gmail.com>
2025-07-16 22:41:08 +0300

    Revert "changed resources"
    This reverts commit 7811f00e3990d7aba7db2baa4c26902c1760366d.

--- pokerv2/src/config/AssetConfig.js
+++ pokerv2/src/config/AssetConfig.js
@@ -76,7 +76,7 @@ export const AssetConfig = {
         
         // Generate card keys dynamically: e.g., '2_of_clubs', 'ace_of_spades'
         getCardKey: (value, suit) => `${value}_of_${suit}`,
-        getCardPath: (value, suit) => `assets/cards_cvg/${value}_of_${suit}.svg`,
+        getCardPath: (value, suit) => `assets/cards/${value}_of_${suit}.png`,
     },
 
     // Media and branding assets
