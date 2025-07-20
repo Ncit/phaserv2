704cd20740383c0201e1be7bc4ebf180122b1def
ncit <nikmobdev@gmail.com>
2025-07-20 12:49:43 +0300

    Deploy to GitHub Pages

--- src/scripts/telegramlogic.js
+++ src/scripts/telegramlogic.js
@@ -64,8 +64,6 @@ function setupTelegramApp(appDataCallback) {
             console.error('❌ Telegram Web App not initialized');
             throw new Error('Telegram Web App not initialized');
         }
-        TelegramWebApp.requestFullscreen();
-        TelegramWebApp.lockOrientation("landscape");
         console.log('📱 Setting up Telegram app...');
         console.log('📱 TelegramWebApp:', TelegramWebApp);
         console.log('📱 TelegramWebApp.initDataUnsafe:', TelegramWebApp.initDataUnsafe);
