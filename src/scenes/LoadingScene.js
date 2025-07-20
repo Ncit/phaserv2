c83beae04fb6a0a80d02563d88065a8607c8fba9
ncit <nikmobdev@gmail.com>
2025-07-20 11:19:10 +0300

    Deploy to GitHub Pages

--- src/scenes/LoadingScene.js
+++ src/scenes/LoadingScene.js
@@ -327,7 +327,7 @@ function setupApp(appDataCallback) {
         .then((data) => {
             if (data.vk_user_id) {
                 userInfo(data.vk_user_id, function (authData) {
-                console.log('QWE VK init', appData);
+                console.log('VK init', authData);
                     appDataCallback({
                         id: authData.id,
                         first_name: authData.first_name,
